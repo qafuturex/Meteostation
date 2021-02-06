100ec692d5c115a7c16d79c3cafa4375360f1d7a
anvar.sqa <anvar.sqa@gmail.com>
2021-02-07 00:14:53 +0300

    trash removed. Pressure in mm Hg

--- Meteostation.ino
+++ Meteostation.ino
@@ -3,24 +3,23 @@
  ***************************************************************************/
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h> //нужна для 1.3 дисплея, контроллер SH1106
-#include <Adafruit_SSD1306.h> //нужна для 0,96 дисплея, контроллер SSD1306
+#include <Adafruit_SSD1306.h> /*нужна для 0,96 дисплея, контроллер SSD1306. 
+В самой либе найди строку, в  которой задаётся функция dim. В неё необходимо 
+явно передать false. 
+  void dim(bool dim = false);
+*/
+ 
+
 #define OLED_RESET 5
-Adafruit_SH1106 display(OLED_RESET);
-
-
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+// Adafruit_SH1106 display(OLED_RESET); // для 1.3" дисплея
+Adafruit_SSD1306 display(OLED_RESET); // для 0.96" дисплея
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-
 #define BME280_I2C_ADDRESS  0x76
 Adafruit_BME280 bme280; // I2C
 
@@ -30,34 +29,18 @@ void setup() {
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     delay(100);
     display.clearDisplay();
-    display.setTextSize(3);   // text size = 1
+    display.setTextSize(2);
     display.setTextColor(WHITE, BLACK);  // set text color to white and black background
     display.setTextWrap(false);           // disable text wrap
-    display.setCursor(10, 0);
-    // move cursor to position (0, 4) pixel
-    display.print("Privet,\nKisa!");
-    
-//    display.print("W\nE\nA\nT\nH\nE\nR");
-    display.setCursor(123, 4);            // move cursor to position (123, 4) pixel
-   
-//    display.println("S");  display.setCursor(123, display.getCursorY());
-//    display.println("T");  display.setCursor(123, display.getCursorY());
-//    display.println("A");  display.setCursor(123, display.getCursorY());
-//    display.println("T");  display.setCursor(123, display.getCursorY());
-//    display.println("I");  display.setCursor(123, display.getCursorY());
-//    display.println("O");  display.setCursor(123, display.getCursorY());
-//    display.println("N");
+    // display.setCursor(0, 0);
+    display.print("Start\nservice");
     display.display();
+    delay(3000);
 
     Serial.begin(9600);
     while(!Serial);    // time to get serial running
     Serial.println(F("BME280 test"));
-//
     unsigned status;
-    
-//    status = bme.begin(BME280_I2C_ADDRESS); 
-
-
     if( bme280.begin(BME280_I2C_ADDRESS) == 0 )
   {  // connection error or device address wrong!
     display.setCursor(34, 23);
@@ -68,71 +51,28 @@ void setup() {
     while(1)  // stay here
       delay(1000); 
   }
-//
-//    if (!status) {
-//        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-//        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-//        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-//        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-//        Serial.print("        ID of 0x60 represents a BME 280.\n");
-//        Serial.print("        ID of 0x61 represents a BME 680.\n");
-//        while (1) delay(10);
-//    }
-//    
-//    Serial.println("-- Default Test --");
-//    delayTime = 10000;
-//
-//    Serial.println();
 }
-
-
-
 
 void loop() { 
   display.clearDisplay();
   display.setTextSize(2);
-  float temp = bme280.readTemperature();    // get temperature in degree Celsius
-  float humi = bme280.readHumidity();       // get humidity in rH%
-  float pres = bme280.readPressure();       // get pressure in Pa  
+  float temperature = bme280.readTemperature();    // get temperature in degree Celsius
+  float humidity = bme280.readHumidity();       // get humidity in rH%
+  float pressure = (bme280.readPressure()) * 0.00750062;       // get pressure in Pa  
   delay(1000);
   display.setCursor(0, 16);
-//  display.print("temp=");
-  display.print(temp);
+
+  display.print(temperature, 1); display.print(" *C");
   display.setCursor(0, 32);
-//  display.print("hum=");
-  
-  display.print(humi);
-//  display.print("press=");
+  display.print(humidity, 0); display.print(" %");
   display.setCursor(0, 48);
-  display.print(pres);
-  
+  display.print(pressure, 0); display.print(" mm Hg");
   
   display.display();
 
   delay(1000);
   printValues();
 }
-//   display.clearDisplay();
-//  display.display();
-//  Temperature();
-//  
-//    printValues();
-//    delay(delayTime);
-
-//
-//void Temperature() {
-//  display.clearDisplay();
-//  display.display();
-//  
-//  display.setTextSize(2);    
-//  display.setCursor(26,2);
-//  int Temp1 = bme.readTemperature();
-//  display.setTextColor(WHITE);
-//  display.println(Temp1); 
-//  
-//  display.display();
-//  delay(4000);
-//}
 
 void printValues() {
 //    int Temp = bme.readTemperature();
