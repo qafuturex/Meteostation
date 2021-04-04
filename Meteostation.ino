93b38bdf0eb6f5366fb5f30c5d68e8f378e128c6
anvar.sqa <anvar.sqa@gmail.com>
2021-04-04 22:46:17 +0300

    small refactoring

--- Meteostation.ino
+++ Meteostation.ino
@@ -1,5 +1,5 @@
 /***************************************************************************
-
+TODO: разнести функции дисплея и метеодатчика по файлам.
  ***************************************************************************/
 
 #include <Wire.h>
@@ -8,31 +8,32 @@
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h> //нужна для 1.3 дисплея, контроллер SH1106
-#include <Adafruit_SSD1306.h> /*нужна для 0,96 дисплея, контроллер SSD1306. 
-В самой либе найди строку, в  которой задаётся функция dim. В неё необходимо 
-явно передать false. 
+//#include <Adafruit_SSD1306.h> //нужна для 0,96 дисплея, контроллер SSD1306. 
+
+/*In Adafruit_SSD1306.h you should find "dim". You need pass to it "false". 
   void dim(bool dim = false);
 */
  
-
-#define OLED_RESET 5
-// Adafruit_SH1106 display(OLED_RESET); // для 1.3" дисплея
-Adafruit_SSD1306 display(OLED_RESET); // для 0.96" дисплея
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define BME280_I2C_ADDRESS  0x76
+#define OLED_RESET 5
+
 Adafruit_BME280 bme280; // I2C
+Adafruit_SH1106 display(OLED_RESET); // for 1.3" display
+//Adafruit_SSD1306 display(OLED_RESET); // for 0.96" display
+
+
+
 
 unsigned long delayTime;
 
 void setup() {
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.begin(SH1106_SWITCHCAPVCC, 0x3C); //here you can don't change to SH1106
     delay(100);
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(WHITE, BLACK);  // set text color to white and black background
     display.setTextWrap(false);           // disable text wrap
-    // display.setCursor(0, 0);
     display.print("Start\nservice");
     display.display();
     delay(3000);
@@ -59,7 +60,6 @@ void loop() {
   float temperature = bme280.readTemperature();    // get temperature in degree Celsius
   float humidity = bme280.readHumidity();       // get humidity in rH%
   float pressure = (bme280.readPressure()) * 0.00750062;       // get pressure in Pa  
-  delay(1000);
   display.setCursor(0, 16);
 
   display.print(temperature, 1); display.print(" *C");
@@ -70,7 +70,7 @@ void loop() {
   
   display.display();
 
-  delay(1000);
+  delay(30000);
   printValues();
 }
 
